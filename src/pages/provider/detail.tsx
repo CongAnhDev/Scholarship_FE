40f976171553070c277b961b8e287df9f703efe8
Trương Quang Quyền <QuangQuyen1305@gmail.com>
2024-09-13 00:12:31 +0700

    Detail Provider

--- src/pages/provider/detail.tsx
+++ src/pages/provider/detail.tsx
@@ -7,14 +7,14 @@ import parse from 'html-react-parser';
 import { Col, Divider, Row, Skeleton } from "antd";
 import { EnvironmentOutlined } from "@ant-design/icons";
 import DetailButton from "./buttondetail";
-import Infor from "./infor";
 
 
 const ClientProviderDetailPage = (props: any) => {
     const [ProviderDetail, setProviderDetail] = useState<IProvider | null>(null);
     const [isLoading, setIsLoading] = useState<boolean>(false);
     const [visible, setVisible] = useState(false)
-    const [visible2, setVisible2] = useState(false)
+    const [visible2, setVisible2] = useState(true)
+    const [visible3, setVisible3] = useState(false)
     let location = useLocation();
     let params = new URLSearchParams(location.search);
     const id = params?.get("id"); // job id
@@ -40,19 +40,21 @@ const ClientProviderDetailPage = (props: any) => {
                 <Row gutter={[20, 20]}>
                     {ProviderDetail && ProviderDetail._id &&
                         <>
+                        <div>
                             <Row gutter={[10, 10]} className={styles["custom-row"]}>
                                 <div style={{ paddingTop: '30px' }}></div>
                                 <Col span={8}><img
                                     alt="example"
                                     src={`${import.meta.env.VITE_BACKEND_URL}/images/Provider/${ProviderDetail?.logo}`}
-                                    style={{ width: '150px', height: '150px', objectFit: 'cover' }}
+                                    style={{ width: '250px', height: '150px', objectFit: 'cover' }}
                                 /></Col>
-                                <Col span={16} style={{ color: '#353535', fontSize: '27px', display: 'flex', alignItems: 'center', justifyContent: 'center', fontWeight: 700 }}><div>
+                                <Col span={16} style={{ color: '#353535', fontSize: '27px', display: 'flex', alignItems: 'center', justifyContent: 'center', fontWeight: 700, paddingLeft:'20px' }}><div>
                                     {ProviderDetail.name}
                                 </div></Col>
 
                             </Row>
                             <DetailButton />
+                            </div>
                             <div style={{ paddingTop: '30px' }}>
                                 <div
                                     className="grid grid-cols-2 px-[16px] c-lg:px-0 c-lg:flex gap-[36px] c-lg:gap-[40px] flex-row flex-wrap justify-between"
@@ -271,6 +273,110 @@ const ClientProviderDetailPage = (props: any) => {
                                         <button
                                             className="flex w-full gap-[8px]"
                                             aria-label="Collapse Tổng quan "
+                                            onClick={() => setVisible2(!visible2)}
+                                            aria-expanded={visible2}
+                                            aria-controls="collapseWidthExample"
+                                            style={{
+                                                border: "0px solid rgb(229, 231, 235)",
+                                                boxSizing: "border-box",
+                                                margin: "0px",
+                                                padding: "0px",
+                                                fontSize: "100%",
+                                                lineHeight: "inherit",
+                                                color: "inherit",
+                                                textTransform: "none",
+                                                appearance: "button",
+                                                backgroundColor: "initial",
+                                                backgroundImage: "none",
+                                                cursor: "pointer",
+                                                fontFamily: "BuenosAiresVN, sans-serif",
+                                                fontWeight: 400,
+                                                display: "flex",
+                                                width: "100%",
+                                                gap: "8px",
+                                            }}
+                                        >
+                                            <span
+                                                className="flex-1 text-left"
+                                                style={{
+                                                    border: "0px solid rgb(229, 231, 235)",
+                                                    boxSizing: "border-box",
+                                                    flex: "1 1 0%",
+                                                    textAlign: "left",
+                                                }}
+                                            >
+                                                Khuôn Viên Trường
+                                            </span>
+                                            <svg
+                                                className="relative top-[3px]"
+                                                height="1em"
+                                                width="1em"
+                                                fill="currentColor"
+                                                stroke="currentColor"
+                                                strokeWidth="0"
+                                                viewBox="0 0 512 512"
+                                                xmlns="http://www.w3.org/2000/svg"
+
+                                                style={{
+                                                    border: "0px solid rgb(229, 231, 235)",
+                                                    boxSizing: "border-box",
+                                                    display: "block",
+                                                    verticalAlign: "middle",
+                                                    position: "relative",
+                                                    top: "3px",
+                                                    transform: visible2 ? 'rotate(0deg)' : 'rotate(-90deg)',
+                                                    transition: 'transform 0.3s ease',
+                                                }}
+                                            >
+                                                <path
+                                                    d="M96 235h320v42H96z"
+                                                    style={{
+                                                        border: "0px solid rgb(229, 231, 235)",
+                                                        boxSizing: "border-box",
+                                                    }}
+                                                />
+
+                                            </svg>
+                                        </button>
+                                    </h3>
+                                    {visible2 && (
+                                                   <div>
+                                                   {ProviderDetail.gallery.length === 1 ? (
+                                                       <img src={ProviderDetail.gallery[0]} alt="Single Image" style={{ width: '100%', height: 'auto' }} />
+                                                   ) : (
+                                                       <div>
+                                                           {ProviderDetail.gallery.map((image: string, index: number) => (
+                                                               <img
+                                                                   key={index}
+                                                                   src={`${import.meta.env.VITE_BACKEND_URL}/images/gallery/`+image}
+                                                                   alt={`Image ${index + 1}`}
+                                                                   style={{ width: '25%', height: 'auto', maxWidth: '300px', maxHeight: '200px', margin: '5px' }}
+                                                               />
+                                                           ))}
+                                                       </div>
+                                                   )}
+                                               </div>
+                                    )}
+                                    <h3
+                                        className="text-para c-md:text-heading-5 flex py-[20px] c-md:py-[24px] border-b font-custom-regular accordion-heading border-transparent"
+                                        style={{
+                                            border: "0px solid rgb(229, 231, 235)",
+                                            boxSizing: "border-box",
+                                            margin: "0px",
+                                            display: "flex",
+                                            borderBottomWidth: "1px",
+                                            borderColor: "rgba(0, 0, 0, 0)",
+                                            paddingTop: "24px",
+                                            paddingBottom: "24px",
+                                            fontSize: "20px",
+                                            lineHeight: 1.3,
+                                            fontFamily: "BuenosAiresVN, sans-serif",
+                                            fontWeight: 400,
+                                        }}
+                                    >
+                                        <button
+                                            className="flex w-full gap-[8px]"
+                                            aria-label="Collapse Tổng quan "
                                             onClick={() => setVisible(!visible)}
                                             aria-expanded={visible}
                                             aria-controls="collapseWidthExample"
@@ -364,8 +470,8 @@ const ClientProviderDetailPage = (props: any) => {
                                         <button
                                             className="flex w-full gap-[8px]"
                                             aria-label="Collapse Tổng quan "
-                                            onClick={() => setVisible2(!visible2)}
-                                            aria-expanded={visible2}
+                                            onClick={() => setVisible3(!visible3)}
+                                            aria-expanded={visible3}
                                             aria-controls="collapseWidthExample"
                                             style={{
                                                 border: "0px solid rgb(229, 231, 235)",
@@ -396,7 +502,7 @@ const ClientProviderDetailPage = (props: any) => {
                                                     textAlign: "left",
                                                 }}
                                             >
-                                                Khuôn Viên Trường
+                                                Cơ Cấu Khoa
                                             </span>
                                             <svg
                                                 className="relative top-[3px]"
@@ -415,7 +521,7 @@ const ClientProviderDetailPage = (props: any) => {
                                                     verticalAlign: "middle",
                                                     position: "relative",
                                                     top: "3px",
-                                                    transform: visible2 ? 'rotate(0deg)' : 'rotate(-90deg)',
+                                                    transform: visible3 ? 'rotate(0deg)' : 'rotate(-90deg)',
                                                     transition: 'transform 0.3s ease',
                                                 }}
                                             >
@@ -430,63 +536,14 @@ const ClientProviderDetailPage = (props: any) => {
                                             </svg>
                                         </button>
                                     </h3>
-                                    {visible2 && (
+                                    {visible3 && (
                                                    <div>
-                                                   {ProviderDetail.gallery.length === 1 ? (
-                                                       <img src={ProviderDetail.gallery[0]} alt="Single Image" style={{ width: '100%', height: 'auto' }} />
-                                                   ) : (
-                                                       <div>
-                                                           {ProviderDetail.gallery.map((image: string, index: number) => (
-                                                               <img
-                                                                   key={index}
-                                                                   src={`${import.meta.env.VITE_BACKEND_URL}/images/gallery/`+image}
-                                                                   alt={`Image ${index + 1}`}
-                                                                   style={{ width: '25%', height: 'auto', maxWidth: '300px', maxHeight: '200px', margin: '5px' }}
-                                                               />
-                                                           ))}
-                                                       </div>
-                                                   )}
+                                                   <p>{ProviderDetail.info}</p>
                                                </div>
                                     )}
+
                                 </div>
                             </div>
-
-                            {/* <Infor /> */}
-
-                            {/* <Col span={24} md={16}>
-                            <img
-                                alt="example"
-                                src={`${import.meta.env.VITE_BACKEND_URL}/images/Provider/${ProviderDetail?.logo}`}
-                                style={{width:'150px',height:'150px',objectFit:'cover'}}
-                            />
-                                <div className={styles["header"]}>
-                                    {ProviderDetail.name}
-                                </div>
-                            <div>
-                                        
-                                    </div>
-                                <div className={styles["location"]}>
-                                    <EnvironmentOutlined style={{ color: '#58aaab' }} />&nbsp;{(ProviderDetail?.address)}
-                                </div>
-
-                                <Divider />
-                                {parse(ProviderDetail?.description ?? "")}
-                            </Col>
-                            <Col span={24} md={8}>
-                                <div style={{ height: 350 ,position:'relative',display: 'flex', flexDirection: 'column',justifyContent: 'space-between'}}>
-                                    <div>
-                                        <img
-                                            alt="example"
-                                            src={`${import.meta.env.VITE_BACKEND_URL}/images/Provider/${ProviderDetail?.logo}`}
-                                            style={{width:'100%',height:'auto',objectFit:'cover'}}
-                                        />
-                                    </div>
-                                    <div>
-                                        {ProviderDetail?.name}
-                                    </div>
-                                </div>
-                            </Col> */}
-
                         </>
                     }
                 </Row>
